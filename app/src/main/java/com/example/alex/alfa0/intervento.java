406c4be41c75da98d870b5e278baacdbc5b083a2
AlexCG97 <ghiurcaalex@gmail.com>
2018-03-07 13:01:36 +0100

    Eseguita correttamente la ricezione del JSON.

--- app/src/main/java/com/example/alex/alfa0/intervento.java
+++ app/src/main/java/com/example/alex/alfa0/intervento.java
@@ -5,9 +5,10 @@ package com.example.alex.alfa0;
  */
 
 public class intervento {
-    private String id ="";
+    private String id = "";
     private String nome = " ";
     private String cognome = " ";
+    private String dataNascita = "";
     private String via = " ";
     private String numero = " ";
     private String citta = " ";
@@ -17,116 +18,159 @@ public class intervento {
     private String chiamata = " ";
     private String codice = " ";
     private String operatore = " ";
+    private String attivo;
 
-    public intervento(){
+    public intervento() {
 
     }
 
-    public intervento(String id, String nome, String cognome, String via, String numero, String citta, String chiamata, String codice){
+    public intervento(String id, String nome, String cognome, String dataNascita, String via, String numero, String citta, String comune, String cap, String data, String chiamata, String codice, String operatore, String attivo) {
         this.id = id;
         this.nome = nome;
         this.cognome = cognome;
+        this.dataNascita = dataNascita;
         this.via = via;
         this.numero = numero;
         this.citta = citta;
+        this.comune = comune;
+        this.cap = cap;
+        this.data = data;
         this.chiamata = chiamata;
         this.codice = codice;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public String getVia() {
-        return via;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public String getCitta() {
-        return citta;
-    }
-
-    public String getComune() {
-        return comune;
-    }
-
-    public String getCap() {
-        return cap;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public String getChiamata() {
-        return chiamata;
-    }
-
-    public String getCodice() {
-        return codice;
-    }
-
-    public String getOperatore() {
-        return operatore;
+        this.operatore = operatore;
+        this.attivo = attivo;
     }
 
     public String getId() {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
     }
 
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
 
+    public String getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(String dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
     public void setVia(String via) {
         this.via = via;
+    }
+
+    public String getNumero() {
+        return numero;
     }
 
     public void setNumero(String numero) {
         this.numero = numero;
     }
 
+    public String getCitta() {
+        return citta;
+    }
+
     public void setCitta(String citta) {
         this.citta = citta;
+    }
+
+    public String getComune() {
+        return comune;
     }
 
     public void setComune(String comune) {
         this.comune = comune;
     }
 
+    public String getCap() {
+        return cap;
+    }
+
     public void setCap(String cap) {
         this.cap = cap;
+    }
+
+    public String getData() {
+        return data;
     }
 
     public void setData(String data) {
         this.data = data;
     }
 
+    public String getChiamata() {
+        return chiamata;
+    }
+
     public void setChiamata(String chiamata) {
         this.chiamata = chiamata;
+    }
+
+    public String getCodice() {
+        return codice;
     }
 
     public void setCodice(String codice) {
         this.codice = codice;
     }
 
+    public String getOperatore() {
+        return operatore;
+    }
+
     public void setOperatore(String operatore) {
         this.operatore = operatore;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getAttivo() {
+        return attivo;
     }
 
+    public void setAttivo(String attivo) {
+        this.attivo = attivo;
+    }
+
+    @Override
+    public String toString() {
+        return "intervento{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", dataNascita='" + dataNascita + '\'' +
+                ", via='" + via + '\'' +
+                ", numero='" + numero + '\'' +
+                ", citta='" + citta + '\'' +
+                ", comune='" + comune + '\'' +
+                ", cap='" + cap + '\'' +
+                ", data='" + data + '\'' +
+                ", chiamata='" + chiamata + '\'' +
+                ", codice='" + codice + '\'' +
+                ", operatore='" + operatore + '\'' +
+                ", attivo='" + attivo + '\'' +
+                '}';
+    }
 }
+
--- app/src/main/java/com/example/alex/alfa0/schedaIntervento.java
+++ app/src/main/java/com/example/alex/alfa0/schedaIntervento.java
@@ -1,8 +1,12 @@
 package com.example.alex.alfa0;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,9 +18,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,8 +42,8 @@ public class schedaIntervento extends AppCompatActivity {
         TVNome = (TextView) findViewById(R.id.TVNome);
         getUsername();
         //JSONObject schedaIntervento = new JSONObject().getJSONObject();
-        String url = "http://10.0.0.238/gestioneambulanze/API_getScheda.php?";
-        getScheda(url);
+        String url = "http://10.0.0.238/gestioneambulanze/API_getScheda.php";
+        getJSON(url);
 
 
 
@@ -46,7 +55,7 @@ public class schedaIntervento extends AppCompatActivity {
         TVNome.setText(j);
         return j;
     }
-
+/*
     private void getScheda(String url){
 
             RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -56,37 +65,7 @@ public class schedaIntervento extends AppCompatActivity {
                 public void onResponse(String response) {
 
 
-                        try {
 
-                            String s = "";
-                            JSONObject ob = new JSONObject(s);
-                            JSONArray arr = ob.getJSONArray("records");
-
-                            for (int i = 0; i < arr.length(); i++) {
-                                JSONObject o = arr.getJSONObject(i);
-                                intervento a = new intervento();
-                                a.setId(o.getString("Id_chiamata"));
-                                Toast.makeText(schedaIntervento.this, a.getId(), Toast.LENGTH_SHORT).show();
-                                TVID.setText(a.getId());
-                                /*
-                                System.out.println((o.getString("ID_Chiamata")));
-                                System.out.println((o.getString("Nome")));
-                                System.out.println((o.getString("Cognome")));
-                                System.out.println((o.getString("Data_di_nascita")));
-                                System.out.println((o.getString("Via")));
-                                System.out.println((o.getString("Numero")));
-                                System.out.println((o.getString("Citta")));
-                                System.out.println((o.getString("Comune")));
-                                System.out.println((o.getString("CAP")));
-                                System.out.println((o.getString("Motivo_chiamata")));
-                                System.out.println((o.getString("Operatore")));
-                                System.out.println((o.getString("codice")));
-                                */
-                            }
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -110,13 +89,92 @@ public class schedaIntervento extends AppCompatActivity {
 
 
         }
+*/
+
+    private void getJSON(final String urlWebService) {
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
 
 
 
 
+    private void loadIntoListView(String json) throws JSONException {
+        //creating a json array from the json string
+        JSONArray jsonArray = new JSONArray(json);
+        //creating a string array for listview
+        String[] schede = new String[jsonArray.length()];
+        intervento[] schedeInterventi = new intervento[jsonArray.length()];
+        //looping through all the elements in json array
+        for (int i = 0; i < jsonArray.length(); i++) {
+            //getting json object from the json array
+            JSONObject obj = jsonArray.getJSONObject(i);
+            //getting the name from the json object and putting it inside string array
+            //schede[i] = obj.getString("name");
+
+            /*schedeInterventi[i].setId(obj.getString("ID_chiamata"));
+            schedeInterventi[i].setNome(obj.getString("Nome"));
+            schedeInterventi[i].setCognome(obj.getString("Cognome"));
+            schedeInterventi[i].setData(obj.getString("Data_di_nascita"));
+            schedeInterventi[i].setVia(obj.getString("Via"));
+            schedeInterventi[i].setNumero(obj.getString("Numero"));
+            schedeInterventi[i].setCap(obj.getString("CAP"));
+            schedeInterventi[i].setChiamata(obj.getString("Motivo_chiamata"));
+            schedeInterventi[i].setOperatore(obj.getString("Operatore"));
+            schedeInterventi[i].setCodice(obj.getString("codice"));
+            schedeInterventi[i].setAttivo(obj.getString("Attivo"));
+            Toast.makeText(this, schedeInterventi[i].toString(), Toast.LENGTH_SHORT).show();*/
 
 
+        }
+        //the array adapter to load data into list
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, schede);
 
-
+        //attaching adapter to listview
+        //listView.setAdapter(arrayAdapter);
+    }
 
 }
+
+
