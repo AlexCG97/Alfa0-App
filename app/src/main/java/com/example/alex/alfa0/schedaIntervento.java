c714c38fe0ce33cb5f771498b37a7f15e3c14e59
AlexCG97 <ghiurcaalex@gmail.com>
2018-04-10 20:44:45 +0200

    Inserita mappa e fixati il fragment.

--- app/src/main/java/com/example/alex/alfa0/schedaIntervento.java
+++ app/src/main/java/com/example/alex/alfa0/schedaIntervento.java
@@ -1,13 +1,20 @@
 package com.example.alex.alfa0;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,14 +25,14 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 
 
-
-public class schedaIntervento extends AppCompatActivity {
-    String Nome, Cognome, Via, Numero, Citta, CAP, Chiamata, Operatore, Codice;
-    TextView TVNome, TVID, TVNomeP, TVCognome, TVVia, TVNumero, TVCitta, TVCap, TVChiamata, TVOperatore, TVCodice, TVData;
-    String Nomee;
-
+public class schedaIntervento extends AppCompatActivity implements OnMapReadyCallback {
+    TextView TVNome, TVID, TVNomeP, TVCognome, TVVia, TVNumero, TVCitta, TVCap, TVChiamata, TVOperatore, TVCodice, TVData, TVStringa;
+    String Nomee, Indirizzo;
+    GoogleMap mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,11 +49,15 @@ public class schedaIntervento extends AppCompatActivity {
         TVOperatore = findViewById(R.id.TVOperatore);
         TVCodice = findViewById(R.id.TVCodice);
         TVData = findViewById(R.id.TVData);
-        getUsername();
-        //JSONObject schedaIntervento = new JSONObject().getJSONObject();
+        TVStringa = findViewById(R.id.TVStringa);
+        Nomee = getUsername();
         String url = "http://192.168.1.21/gestioneambulanze/API_getScheda.php";
         getJSON(url);
-        Nomee = getUsername();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync( this);
+
+
     }
 
     private String getUsername() {
@@ -54,6 +65,11 @@ public class schedaIntervento extends AppCompatActivity {
         String j = i.getStringExtra("Username");
         TVNome.setText(j);
         return j;
+    }
+
+
+    private String getIndirizzo(){
+        return "Via " + TVVia.getText().toString() + TVNumero.getText().toString() + TVCitta.getText().toString() + TVCap.getText().toString();
     }
 
     private void getJSON(final String urlWebService) {
@@ -67,9 +83,9 @@ public class schedaIntervento extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
-                //Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
                 try {
                     loadIntoTextView(s);
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -77,10 +93,7 @@ public class schedaIntervento extends AppCompatActivity {
 
             @Override
             protected String doInBackground(Void... voids) {
-
                 try {
-
-                    String nomee = getUsername();
                     URL url = new URL(urlWebService);
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
                     con.setRequestMethod("POST");
@@ -98,33 +111,25 @@ public class schedaIntervento extends AppCompatActivity {
                     os.close();
                     con.connect();
 
-
                     StringBuilder sb = new StringBuilder();
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
                     String json;
 
                     while ((json = bufferedReader.readLine()) != null) {
-
                         sb.append(json + "\n");
                     }
-
                     return sb.toString().trim();
                 } catch (Exception e) {
                     return null;
                 }
-
             }
         }
-
         GetJSON getJSON = new GetJSON();
         getJSON.execute();
     }
 
 
-
-
     private void loadIntoTextView(String json) throws JSONException {
-
         JSONArray jsonArray = new JSONArray(json);
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject obj = jsonArray.getJSONObject(i);
@@ -138,8 +143,72 @@ public class schedaIntervento extends AppCompatActivity {
             TVOperatore.setText(obj.getString("Operatore"));
             TVCodice.setText(obj.getString("codice"));
             TVData.setText(obj.getString("Data_di_nascita"));
+            Indirizzo = "Via " + TVVia.getText().toString() + " " + TVNumero.getText().toString() + " " + TVCitta.getText().toString() + " " + TVCap.getText().toString();
+            TVStringa.setText(Indirizzo);
+            //Toast.makeText(this, TVStringa.getText().toString(), Toast.LENGTH_SHORT).show();
         }
     }
+
+
+    public LatLng getLocationFromAddress(String strAddress) {
+        /**
+         * A class for handling geocoding and reverse geocoding.
+         * Geocoding is the process of transforming a street address or other description of a location into a (latitude, longitude) coordinate.
+         * Reverse geocoding is the process of transforming a (latitude, longitude) coordinate into a (partial) address.
+         * The amount of detail in a reverse geocoded location description may vary,
+         * for example one might contain the full street address of the closest building,
+         * while another might contain only a city name and postal code.
+         *
+         * See more at: https://developer.android.com/reference/android/location/Geocoder.html
+         */
+        Geocoder coder = new Geocoder(this, Locale.getDefault());
+
+        //A list because of getFromLocationName will return a list of address depending on how many result I want
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            /**
+             * List<Address> getFromLocationName (String locationName, int maxResults)             *
+             * | locationName | String: |           a user-supplied description of a location                      |
+             * | maxResults	  |   int:  |max number of results to return. Smaller numbers (1 to 5) are recommended |
+             * See more at: https://developer.android.com/reference/android/location/Geocoder.html
+             */
+            address = coder.getFromLocationName(strAddress, 2);
+            if (address == null) {
+                return null;
+            }            //I take just the first result even if I've got list of 5 different LAT;LNG results
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+            return p1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return p1;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        /*LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        //LatLng myAddressCoordinates = getLocationFromAddress(TVStringa.toString());
+        //Toast.makeText(this, myAddressCoordinates.toString(), Toast.LENGTH_SHORT).show();
+
+        //mapView.addMarker(new MarkerOptions().position(myAddressCoordinates).title(Indirizzo));
+        //mapView.moveCamera(CameraUpdateFactory.newLatLngZoom(myAddressCoordinates, 16));
+    }
 }
+
 
 
